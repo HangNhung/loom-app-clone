aea883d83738f8290100efb5f26bff86fec0c2f3
hang nhung <hangbichnhung@gmail.com>
2024-11-06 13:34:32 +0700

    feat(hook): create useQueryData hook

---
+++ src/hooks/useQueryData.ts
@@ -0,0 +1,9 @@
+import { QueryKey, QueryFunction, useQuery } from "@tanstack/react-query";
+
+export const useQueryData = (queryKey: QueryKey, queryFn: QueryFunction) => {
+  const { data, isPending, isFetched, refetch, isFetching } = useQuery({
+    queryKey,
+    queryFn,
+  });
+  return { data, isPending, isFetched, refetch, isFetching };
+};
